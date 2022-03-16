97d202d9da4c8a5fcacbdd479b3983c7f565cf7c
ingtshan <rongcanyo@gmail.com>
2022-03-16 13:57:30 +0800

    fix(vertico): 🐛 minibuffer of posframe
    - if unfocused quit minibuffer

--- modules/completion/vertico/+posframe.el
+++ modules/completion/vertico/+posframe.el
@@ -16,13 +16,21 @@
   (vertico-posframe-mode 1)
   :custom
   (
-  (vertico-posframe-poshandler
-   #'posframe-poshandler-p0.5p0-to-f0.5p1
-   ;;#'posframe-poshandler-point-top-left-corner
-   )
-  (vertico-posframe-font "Sarasa Mono SC Nerd 15")
-  (vertico-posframe-parameters
-   ;;nil
-   ;;背景RGB (51,62,80)#BDC0C5，前景(189,192,197)#333E50
-   '((foreground-color . "#BDC0C5")
-     (background-color . "#333E50")))))
+   (vertico-posframe-poshandler
+    #'posframe-poshandler-p0.5p0-to-f0.5p1
+    ;;#'posframe-poshandler-point-top-left-corner
+    )
+   (vertico-posframe-font "Sarasa Mono SC Nerd 15")
+   (vertico-posframe-parameters
+    ;;nil
+    ;;背景RGB (51,62,80)#BDC0C5，前景(189,192,197)#333E50
+    '((foreground-color . "#BDC0C5")
+      (background-color . "#333E50")))))
+
+(defun +vertico-stop-using-minibuffer ()
+  "kill the minibuffer"
+  (when (and (>= (recursion-depth) 1)
+             (active-minibuffer-window))
+    (abort-recursive-edit)))
+;; kill minibuffer while unfocus
+(add-hook 'mouse-leave-buffer-hook '+vertico-stop-using-minibuffer)
