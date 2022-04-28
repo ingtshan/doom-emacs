eaa724154d1fecd649cdaab86260a2f450386db8
ingtshan <rongcanyo@gmail.com>
2022-04-28 13:19:47 +0800

    fix(vertico): posframe rest to default

--- modules/completion/vertico/+posframe.el
+++ modules/completion/vertico/+posframe.el
@@ -13,19 +13,7 @@
 
 (use-package! vertico-posframe
   :config
-  (vertico-posframe-mode 1)
-  :custom
-  (
-   (vertico-posframe-poshandler
-    #'posframe-poshandler-p0.5p0-to-f0.5p1
-    ;;#'posframe-poshandler-point-top-left-corner
-    )
-   (vertico-posframe-font "Sarasa Mono SC Nerd 15")
-   (vertico-posframe-parameters
-    ;;nil
-    ;;背景RGB (51,62,80)#BDC0C5，前景(189,192,197)#333E50
-    '((foreground-color . "#BDC0C5")
-      (background-color . "#333E50")))))
+  (vertico-posframe-mode 1))
 
 (defun +vertico-stop-using-minibuffer ()
   "kill the minibuffer"
